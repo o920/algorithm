2a18ec7863322da3c146279f95109555be32d298
o920 <42775031+o920@users.noreply.github.com>
2020-02-28 16:14:08 +0900

    Add files via upload

---
+++ 1463.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[1000001] = {0,};
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 2; i <= N; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3==0) dp[i] = min(dp[i], dp[i / 3]+1);
+		if (i % 2==0) dp[i] = min(dp[i], dp[i / 2]+1);
+	}
+	cout << dp[N] << endl;
+}
