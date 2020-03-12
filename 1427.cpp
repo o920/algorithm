73ebbf896d84cc6198db8a48a49e3e3c2b3a8216
o920 <42775031+o920@users.noreply.github.com>
2020-03-12 09:37:25 +0900

    Add files via upload

---
+++ 1427.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	sort(s.begin(), s.end());
+	reverse(s.begin(), s.end());
+	cout << s << endl;
+}
