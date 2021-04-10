d4b9d33be69fb009c9afe6a3e93054a548f6b587
Sungyeon Park <42775031+o920@users.noreply.github.com>
2021-04-10 19:07:51 +0900

    Update 단어 변환.py

--- programmers/단어 변환.py
+++ programmers/단어 변환.py
@@ -1,28 +1,16 @@
-from collections import deque
-
-def solution(begin, target, words):
-    if target not in words : return 0
-    
+def solution(begin, target, words) :
     answer = 0
-    check = [0] * (len(words)+1)
-    visited = [False] * (len(words)+1)
-    words.append(begin)
-    
-    def function(index) :
-        queue = deque()
-        queue.append(index)
-        visited[index] = True
-        while queue :
-            v = queue.popleft()
-            if words[v] == target : return check[v]
-            count = [0] * len(words)
-            for i in range(len(words)) :
-                count[i] = [True for x, y in zip(words[v],words[i]) if x != y]
-            for i in range(len(count)) :
-                if len(count[i]) == 1 and not visited[i] :
-                    visited[i] = True
-                    check[i] = check[v]+1
-                    queue.append(i)
-    a = function(len(words)-1) 
-    if a : return a
-    else : return 0
+    queue = [begin]
+    while True :
+        temp = []
+        for word1 in queue :
+            if word1 == target : return answer
+            for i in range(len(words)-1, -1, -1) :
+                word2 = words[i]
+                cnt = 0
+                for a, b in zip(word1, word2) :
+                    if a != b : cnt += 1
+                if cnt  == 1 : temp.append(words.pop(i))
+        if not temp : return 0
+        queue = temp
+        answer += 1
